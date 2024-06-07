75887dd774b1405e0fcecd6fae74ee071dbaf74c
Gorenkov A.Y. <agorenkov@mininform74.ru>
2024-06-07 13:33:29 +0500

    formatting

--- src/modules/users/user-list.tsx
+++ src/modules/users/user-list.tsx
@@ -1,24 +1,28 @@
-import {memo, useMemo, useState} from "react";
-import {useAppDispatch, useAppSelector} from "../../store.ts";
-import {UserDeselectAction, UserId, UserSelectAction} from "./users.slice.ts";
+import { memo, useMemo, useState } from "react";
+import { useAppDispatch, useAppSelector } from "../../store.ts";
+import { UserDeselectAction, UserId, UserSelectAction } from "./users.slice.ts";
 
 export function UserList() {
-  const [sortType, setSortType] = useState<"asc" | "desc">("asc")
+  const [sortType, setSortType] = useState<"asc" | "desc">("asc");
 
-  const ids = useAppSelector(state => state.users.ids);
-  const entities = useAppSelector(state => state.users.entities);
-  const selectedUserId = useAppSelector(state => state.users.selectedUserId);
+  const ids = useAppSelector((state) => state.users.ids);
+  const entities = useAppSelector((state) => state.users.entities);
+  const selectedUserId = useAppSelector((state) => state.users.selectedUserId);
 
-  const sortedUsers = useMemo(() => ids
-    .map(id => entities[id])
-    .sort((a, b) => {
-      switch (sortType) {
-        case "asc":
-          return a!.name.localeCompare(b!.name);
-        case "desc":
-          return b!.name.localeCompare(a!.name);
-      }
-    }), [ids, entities, sortType]);
+  const sortedUsers = useMemo(
+    () =>
+      ids
+        .map((id) => entities[id])
+        .sort((a, b) => {
+          switch (sortType) {
+            case "asc":
+              return a!.name.localeCompare(b!.name);
+            case "desc":
+              return b!.name.localeCompare(a!.name);
+          }
+        }),
+    [ids, entities, sortType],
+  );
 
   return (
     <div className="flex flex-col items-center">
@@ -26,29 +30,36 @@ export function UserList() {
         <>
           <div className="flex flex-col items-center justify-between">
             <div className="flex flex-row items-center">
-              <button onClick={() => setSortType('asc')}>Asc</button>
-              <button onClick={() => setSortType('desc')}>Desc</button>
+              <button onClick={() => setSortType("asc")}>Asc</button>
+              <button onClick={() => setSortType("desc")}>Desc</button>
             </div>
           </div>
           <ul className="flex flex-col">
-            {sortedUsers.map((user) => (<UserListItem key={user!.id} userId={user!.id}/>))}
+            {sortedUsers.map((user) => (
+              <UserListItem key={user!.id} userId={user!.id} />
+            ))}
           </ul>
         </>
       ) : (
         <SelectedUser userId={selectedUserId} />
       )}
-    </div>)
+    </div>
+  );
 }
 
-const UserListItem = memo(function UserListItem({ userId }: { userId: UserId }) {
-  const user = useAppSelector(state => state.users.entities[userId]);
+const UserListItem = memo(function UserListItem({
+  userId,
+}: {
+  userId: UserId;
+}) {
+  const user = useAppSelector((state) => state.users.entities[userId]);
   const dispatch = useAppDispatch();
 
   const handleUserClick = () => {
     dispatch({
       type: "USER_SELECT",
-      payload: { userId: user!.id }
-    } satisfies UserSelectAction)
+      payload: { userId: user!.id },
+    } satisfies UserSelectAction);
   };
 
   return (
@@ -59,23 +70,20 @@ const UserListItem = memo(function UserListItem({ userId }: { userId: UserId }) 
 });
 
 function SelectedUser({ userId }: { userId: UserId }) {
-  const user = useAppSelector(state => state.users.entities[userId]);
+  const user = useAppSelector((state) => state.users.entities[userId]);
 
   const dispatch = useAppDispatch();
   const handleDeselectUserClick = () => {
     dispatch({
-      type: "USER_DESELECT"
+      type: "USER_DESELECT",
     } satisfies UserDeselectAction);
   };
 
   return (
     <div className="flex flex-col items-center">
-      <button onClick={handleDeselectUserClick}>
-        Back
-      </button>
+      <button onClick={handleDeselectUserClick}>Back</button>
       <h2 className="text-3xl">{user!.name}</h2>
       <p className="text-xl">{user!.description}</p>
     </div>
-  )
+  );
 }
-
